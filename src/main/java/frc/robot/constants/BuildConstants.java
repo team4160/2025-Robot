761f4e1f5c2a97b15df71c8fb90701b47fcccf93
WindingMotor <kid.subudhi@gmail.com>
2025-01-06 14:10:51 -0500

    Fixed vendordeps

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -12,12 +12,12 @@ public final class BuildConstants {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "SwerveDrive2025";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 25;
-	public static final String GIT_SHA = "86731fa5b99debfd39bfccb543864411412d9b50";
-	public static final String GIT_DATE = "2025-01-05 18:35:25 EST";
+	public static final int GIT_REVISION = 26;
+	public static final String GIT_SHA = "ed63665c4fde73324218568e5ff8660327063228";
+	public static final String GIT_DATE = "2025-01-05 19:12:21 EST";
 	public static final String GIT_BRANCH = "main";
-	public static final String BUILD_DATE = "2025-01-05 18:55:22 EST";
-	public static final long BUILD_UNIX_TIME = 1736121322804L;
+	public static final String BUILD_DATE = "2025-01-06 12:59:45 EST";
+	public static final long BUILD_UNIX_TIME = 1736186385230L;
 	public static final int DIRTY = 1;
 
 	private BuildConstants() {}
